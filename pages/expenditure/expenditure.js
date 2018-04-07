98fd9e9fcabe737827e5b54a5a339607c5abce92
zhengcg <1083466294@qq.com>
2018-04-07 20:36:24 +0800

    在家做的

--- pages/expenditure/expenditure.js
+++ pages/expenditure/expenditure.js
@@ -1,72 +1,96 @@
+// pages/expenditure/expenditure.js
+var wxCharts = require('../../utils/wxcharts.js');
+var pieChart = null;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    autoplay: true,
-    interval: 5000,
-    duration: 1000
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    pieChart = new wxCharts({
+      animation: true,
+      canvasId: 'pieCanvas',
+      type: 'pie',
+      series: [{
+        name: '医药费',
+        data: 78,
+      }, {
+        name: '住院费',
+        data: 56,
+      }, {
+        name: '挂号费',
+        data: 32,
+      }, {
+        name: '诊费',
+        data: 79,
+      }],
+      width: windowWidth,
+      height: 300,
+      dataLabel: true
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+  
   }
 })
