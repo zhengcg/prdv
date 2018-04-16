b87a171c503aa4f54f5fdb9df600dea32187aaf3
zhengcg <1083466294@qq.com>
2018-04-16 15:06:43 +0800

    周一新提交

---
+++ pages/addVaccine/addVaccine.js
@@ -0,0 +1,95 @@
+var app = getApp();
+var header = app.globalData.header;
+var api = app.globalData.api;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "date": ""
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.checkToken()
+
+  },
+
+  // 购药时间
+  bindDateChange: function (e) {
+    var _this = this;
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  checkToken: function () {
+    if (wx.getStorageSync('token')) {
+
+
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '登录过期了，请重新登录！',
+        showCancel: false,
+        success: function (res) {
+          wx.navigateTo({
+            url: '../login/login'
+          })
+        }
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/vaccineRecord/vaccineRecord.js
+++ pages/vaccineRecord/vaccineRecord.js
@@ -7,10 +7,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "showMember": {},
-    "members": [],
-    "index": 0,
-    "membersArray": []
+      members: [],
+      index: 0,
+      page: 1,
+      number: 20,
+      list: []
 
   },
 
@@ -24,15 +25,14 @@ Page({
   // 切换家属
   bindPickerChange: function (e) {
     var self = this;
-    this.setData({
-      index: e.detail.value,
-      showMember: self.data.members[e.detail.value],
+    self.setData({
+      index: parseInt(e.detail.current)
     })
   },
   checkToken: function () {
     if (wx.getStorageSync('token')) {
       this.getMembers();
-      // 添加就诊（一进来先掉一次，然后实际添加数据调用修改接口）
+      
 
 
     } else {
@@ -65,35 +65,11 @@ Page({
       success: function (res) {
         try { wx.hideLoading() } catch (err) { console.log("当前微信版本不支持") }
         if (res.data.code == 200) {
-          var array = []
-          for (var i = 0; i < res.data.data.length; i++) {
-            if (res.data.data[i].relation == 1) {
-              array.push("本人")
-            } else if (res.data.data[i].relation == 2) {
-              array.push("儿子")
-            } else if (res.data.data[i].relation == 3) {
-              array.push("女儿")
-            } else if (res.data.data[i].relation == 4) {
-              array.push("父亲")
-            } else if (res.data.data[i].relation == 5) {
-              array.push("母亲")
-            } else if (res.data.data[i].relation == 6) {
-              array.push("配偶")
-            } else {
-              array.push("其他")
-            }
 
-          }
           _this.setData({
-            members: res.data.data,
-            membersArray: array
+            members: res.data.data
           })
-          if (!_this.data.showMember.id) {
-            _this.setData({
-              showMember: res.data.data[0]
-            })
-
-          }
+          _this.getList(res.data.data[0].id);
 
         } else {
           wx.showToast({
@@ -114,6 +90,51 @@ Page({
       }
     })
   },
+  getList: function (id) {
+    var self = this;
+    try {
+      wx.showLoading({
+        title: '加载中',
+      })
+    } catch (err) {
+      console.log("当前微信版本不支持")
+    }
+    wx.request({
+      url: api + 'Coreout/remindYm', //仅为示例，并非真实的接口地址
+      data: {
+        number: self.data.number,
+        page: self.data.page,
+        session_3rd: wx.getStorageSync('token'),
+        m_id:id
+      },
+      method: 'GET',
+      success: function (res) {
+        try { wx.hideLoading() } catch (err) { console.log("当前微信版本不支持") }
+        if (res.data.code == 200) {
+          if (res.data.data.length) {
+
+            self.setData({
+              page: self.data.page + 1,
+              list: self.data.list.concat(res.data.data)
+            })
+          } else {
+            wx.showToast({
+              title: '没有了！',
+              icon: 'fail',
+              duration: 2000
+            })
+          }
+        } else {
+          wx.showToast({
+            title: "报错了",
+            icon: 'fail',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -154,6 +175,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    this.getList()
 
   },
 
